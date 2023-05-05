bb716489215ee56ff8bb1d48a10c60a1ddd3558c
Russel Musasa <russelmusasa@gmail.com>
2023-05-05 17:51:04 +0100

    Regex - Match literal String with Different Possibilities

--- regular-expressions.js
+++ regular-expressions.js
@@ -5,3 +5,10 @@ let myRegex = /Hello/;
 let result = myRegex.test(myString); // Find if myString contains the String "Hello"
 console.log(myString);
 console.log('Does sentence above contain "Hello"?', result);
+
+// Match a Literal String with Different Possibilities 
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+result = petRegex.test(petString);
+console.log(petString);
+console.log(result);
