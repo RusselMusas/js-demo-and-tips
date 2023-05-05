7e75e3ac4f88087f9d2196b66f1ea38336634749
Russel Musasa <russelmusasa@gmail.com>
2023-05-05 17:42:51 +0100

    Regex - Match Literal in String

---
+++ regular-expressions.js
@@ -0,0 +1,7 @@
+// Regular expressions samples
+// Note Literal match is case sensitive
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Find if myString contains the String "Hello"
+console.log(myString);
+console.log('Does sentence above contain "Hello"?', result);
